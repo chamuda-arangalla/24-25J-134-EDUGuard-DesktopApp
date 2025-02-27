527547bd3abaafe80a935a09d0373675c63fcefc
chamuda-arangalla <chamudaarangalla@gmail.com>
2025-02-27 07:32:47 +0530

    Remove old model output saving funtions

--- EDUGuard_DesktopApp/PyFiles/utils/mongodb_util.py
+++ EDUGuard_DesktopApp/PyFiles/utils/mongodb_util.py
@@ -25,43 +25,26 @@ def authenticate_user(email):
         print(f"Error authenticating user: {e}")
         return None
 
-def save_posture_data(email, posture_data):
-    """
-    Save posture data to the user's record in MongoDB.
-    :param email: User's email address.
-    :param posture_data: Posture data to save.
-    """
-    try:
-        user = authenticate_user(email)
-        if not user:
-            return
+# def save_posture_data(email, posture_data):
+#   """
+#    Save posture data to the user's record in MongoDB.
+#   :param email: User's email address.
+#    :param posture_data: Posture data to save.
+#    """
+#   try:
+#        user = authenticate_user(email)
+#        if not user:
+#            return
+#
+#        users_collection.update_one(
+#            {"Email": email},
+#            {"$push": {"PostureData": posture_data}}
+#        )
+#        print(f"Saved posture data for {email}: {posture_data}")
+#    except Exception as e:
+#        print(f"Error saving posture data: {e}")
 
-        users_collection.update_one(
-            {"Email": email},
-            {"$push": {"PostureData": posture_data}}
-        )
-        print(f"Saved posture data for {email}: {posture_data}")
-    except Exception as e:
-        print(f"Error saving posture data: {e}")
 
-def save_stress_data(email, stress_data):
-    """
-    Save posture data to the user's record in MongoDB.
-    :param email: User's email address.
-    :param stress_data: Stress data to save.
-    """
-    try:
-        user = authenticate_user(email)
-        if not user:
-            return
-
-        users_collection.update_one(
-            {"Email": email},
-            {"$push": {"StressData": stress_data}}
-        )
-        print(f"Saved stress data for {email}: {stress_data}")
-    except Exception as e:
-        print(f"Error saving stress data: {e}")
 
 def update_posture_outputs(progress_report_id, new_outputs):
     """
