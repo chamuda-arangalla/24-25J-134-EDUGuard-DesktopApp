da24acf749a9f02964e7a197bc120133bbe047be
chamuda-arangalla <chamudaarangalla@gmail.com>
2025-03-12 22:05:14 +0530

    Created CustomMessageBox for notification.

---
+++ EDUGuard_DesktopApp/Utilities/CustomMessageBox.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace EDUGuard_DesktopApp.Utilities
+{
+    public partial class CustomMessageBox : Window
+    {
+        public CustomMessageBox()
+        {
+            InitializeComponent();
+        }
+
+        // Constructor that takes a string parameter
+        public CustomMessageBox(string message)
+        {
+            InitializeComponent();
+            MessageTextBlock.Text = message;
+
+            // Position the window at the bottom-right corner
+            var workingArea = SystemParameters.WorkArea;
+            this.Left = workingArea.Right - this.Width - 10;   // 10px offset from the right edge
+            this.Top = workingArea.Bottom - this.Height - 10;   // 10px offset from the bottom edge
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
